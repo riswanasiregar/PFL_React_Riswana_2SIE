244e03649f2ecc550c2819d3a9d966fa082eb077
riswanasiregar <riswana24si@mahasiswa.pcr.ac.id>
2026-04-06 14:12:42 +0700

    perubahan

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Agnes Jesisca</h1>
+      <h1>Riswana</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
