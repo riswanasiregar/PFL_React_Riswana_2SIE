21c31474f72ca3f443c157c4c21788e0c2ffb5a9
riswanasiregar <riswana24si@mahasiswa.pcr.ac.id>
2026-04-06 15:37:39 +0700

    update_searchandfilter

--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -1,11 +1,9 @@
 import { createRoot } from "react-dom/client";
 import "./tailwind.css";
-import FrameworkList from "./FrameworkList";
+import FrameworkList from "./FrameworkListSearchFilter";
 
-
-createRoot(document.getElementById("root"))
-  .render(
-    <div>
-      <FrameworkList/>
-    </div>
-  );
+createRoot(document.getElementById("root")).render(
+  <div>
+    <FrameworkList />
+  </div>
+);
