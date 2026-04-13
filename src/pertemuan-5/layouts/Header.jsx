d9ad5e0d56e65558869c85b14a49eef8f4926c96
riswanasiregar <riswana24si@mahasiswa.pcr.ac.id>
2026-04-13 20:44:48 +0700

    pertemuan5_tugas

--- src/pertemuan-5/layouts/Header.jsx
+++ src/pertemuan-5/layouts/Header.jsx
@@ -4,34 +4,33 @@ import { SlSettings } from "react-icons/sl";
 
 export default function Header() {
   return (
+    /* Terapkan layout dan flexbox untuk div header-container */
     <div
       id="header-container"
       className="flex justify-between items-center p-4"
     >
-      {/* Search Bar */}
       <div id="search-bar" className="relative w-full max-w-lg">
         <input
           id="search-input"
-          className="border border-gray-100 p-2 pr-10 bg-white w-full max-w-lg rounded-md outline-none"
           type="text"
           placeholder="Search Here..."
+          className="border border-gray-100 p-2 pr-10 bg-white w-full max-w-lg rounded-md outline-none"
         />
-
+        {/* 3️⃣ Styling search-icon */}
         <FaSearch
           id="search-icon"
           className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-300"
         />
       </div>
 
-      {/* Icon & Profile Section */}
+      {/*  Icon & Profile Section (icons-container) */}
       <div id="icons-container" className="flex items-center space-x-4">
-        {/* Notification */}
+        {/* Notification Icon */}
         <div
           id="notification-icon"
           className="relative p-3 bg-blue-100 rounded-2xl text-blue-500 cursor-pointer"
         >
           <FaBell />
-
           <span
             id="notification-badge"
             className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-blue-200 rounded-full px-2 py-1 text-xs"
@@ -40,15 +39,15 @@ export default function Header() {
           </span>
         </div>
 
-        {/* Chart */}
+        {/*  Chart Icon */}
         <div
           id="chart-icon"
-          className="p-3 bg-blue-100 rounded-2xl cursor-pointer"
+          className="p-3 bg-blue-100 rounded-2xl cursor-pointer text-xl"
         >
           <FcAreaChart />
         </div>
 
-        {/* Settings */}
+        {/*  Settings Icon */}
         <div
           id="settings-icon"
           className="p-3 bg-red-100 rounded-2xl text-red-500 cursor-pointer"
@@ -61,14 +60,14 @@ export default function Header() {
           id="profile-container"
           className="flex items-center space-x-4 border-l pl-4 border-gray-300"
         >
-          <span id="profile-text">
-            Hello, <b>Fikri Muhaffizh</b>
+          <span id="profile-text" className="text-sm">
+            Hello, <b className="text-gray-900">Riswana Siregar</b>
           </span>
-
           <img
             id="profile-avatar"
-            src="https://avatar.iran.liara.run/public/28"
+            src="/img/memo_18.png"
             className="w-10 h-10 rounded-full"
+            alt="Profile"
           />
         </div>
       </div>
